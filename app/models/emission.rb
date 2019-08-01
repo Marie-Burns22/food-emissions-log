bfb3a510e44739c70032615a8e0a9e85b926acf6
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-08-01 11:43:32 -0700

    Add and edit div on all views to align text.

--- app/models/emission.rb
+++ app/models/emission.rb
@@ -8,10 +8,10 @@ class Emission < ApplicationRecord
 
   #accepts_nested_attributes_for :food
 
-  scope :kg_unit, -> { where(unit: "kg CO2e per lb of food") }
-  scope :lb_unit, -> { where(unit: "lbs of CO2e per serving") }
-  scope :g_unit, -> { where(unit: "g of CO2e per serving")}
-  scope :kg_protein_unit, -> { where(unit: "kg of CO2 per 50 g of protein")}
+  # scope :kg_unit, -> { where(unit: "kg CO2e per lb of food") }
+  # scope :lb_unit, -> { where(unit: "lbs of CO2e per serving") }
+  # scope :g_unit, -> { where(unit: "g of CO2e per serving")}
+  # scope :kg_protein_unit, -> { where(unit: "kg of CO2 per 50 g of protein")}
 
   scope :category, -> (category) {where("food.category LIKE ?", category)}
   scope :food, -> (food) {where("food LIKE ?", food)}
